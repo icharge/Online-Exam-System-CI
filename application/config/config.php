df25a554ce1aff17f8f7e873d558f52e2fef4287
iCharge <charge_n@hotmail.com>
2014-08-27 16:40:28 +0700

    แก้ไข Session Expiration
    แม่งบัค logout เอง  แก้ยังไงไม่รู้
    ปรับเป็น 0 ไว้  ไม่มีวันหมดอายุ

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'iCharge-Project57-CI';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
