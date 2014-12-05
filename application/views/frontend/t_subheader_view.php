f228dae58c3a538c17785fb289597c60a7fdcc33
icharge <charge_n@hotmail.com>
2014-12-05 19:51:53 +0700

    ปรับ class

--- application/views/frontend/t_subheader_view.php
+++ application/views/frontend/t_subheader_view.php
@@ -1,7 +1,7 @@
 	<div id="subheader">
 		<div class="container">
 			<div class="row">
-				<div class="span12">
+				<div class="col-md-12">
 					<h1><?php echo ($title!=''?$title:$this->misc->getClassName());?></h1>
 					<span><?php echo ($subtitle!=''?$subtitle:'');?></span>
 				</div>
